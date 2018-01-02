7c8cea77ef3faa7e9db10564a62f5020db563848
Danny <danny.eck@osi.ca.gov>
2018-01-02 13:39:04 -0800

    Add moxios, improve test coverage

--- app/javascript/dashboard/DashboardContainer.test.js
+++ app/javascript/dashboard/DashboardContainer.test.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import axios from 'axios';
+import moxios from 'moxios';
 import { shallow } from 'enzyme';
 import DashboardContainer from './DashboardContainer';
 import Caseload from '../_components/Caseload';
@@ -26,6 +28,77 @@ describe('<DashboardContainer />', () => {
       const wrapper = shallow(<DashboardContainer />);
       const renderEmptyCaseload = wrapper.instance().renderEmptyCaseload;
       expect(renderEmptyCaseload().type).toEqual(Alert);
+    });
+  });
+
+  describe('data access request methods', () => {
+    beforeEach(() => moxios.install());
+
+    afterEach(() => moxios.uninstall());
+
+    describe('#fetchReferrals()', () => {
+      it('fetches referrals from API', () => {
+        moxios.stubRequest('/api/referrals/0Ki', {
+          status: 200,
+          response: [
+            {
+              identifier: '1',
+              referral_name: 'ref-1',
+              assignment_type: 'assignmentType',
+            },
+            {
+              identifier: '2',
+              referral_name: 'ref-2',
+              assignment_type: 'assignmentType',
+            },
+            {
+              identifier: '3',
+              referral_name: 'ref-3',
+              assignment_type: 'assignmentType',
+            },
+          ],
+        });
+        const wrapper = shallow(<DashboardContainer />);
+        wrapper
+          .instance()
+          .fetchReferrals()
+          .then(duh => console.log(duh));
+        // const wrapper = shallow(<DashboardContainer />, {
+        //   lifecycleExperimental: true,
+        // });
+      });
+    });
+
+    describe('#fetchCases()', () => {
+      it('fetches cases from API', () => {
+        moxios.stubRequest('/api/cases/0Ki/index', {
+          status: 200,
+          response: [
+            {
+              identifier: '1',
+              case_name: 'case-1',
+              assignment_type: 'assignmentType',
+            },
+            {
+              identifier: '2',
+              case_name: 'case-2',
+              assignment_type: 'assignmentType',
+            },
+            {
+              identifier: '3',
+              case_name: 'case-3',
+              assignment_type: 'assignmentType',
+            },
+          ],
+        });
+        const wrapper = shallow(<DashboardContainer />);
+        wrapper
+          .instance()
+          .fetchCases()
+          .then(duh => {
+            console.log(duh);
+          });
+      });
     });
   });
 });
