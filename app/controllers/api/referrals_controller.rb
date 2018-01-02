200a050ca166ecc111ee48ac393a188e124fe4a8
Danny <danny.eck@osi.ca.gov>
2018-01-02 14:14:14 -0800

    Fix more code climate warnings

--- app/controllers/api/referrals_controller.rb
+++ app/controllers/api/referrals_controller.rb
@@ -3,8 +3,8 @@
 module Api
   class ReferralsController < ActionController::API
     def referrals_by_user
-      conn = Faraday.new(:url => 'http://case-api:8080') do |faraday|
-        faraday.adapter  Faraday.default_adapter 
+      conn = Faraday.new( url => 'http://case-api:8080' ) do |faraday|
+        faraday.adapter Faraday.default_adapter
       end
 
       res = conn.get do |req|
