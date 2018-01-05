d4abec74a2593e08bd75bc1fbf5e69dd40072b25
Danny Eck <daniel.eck@accenture.com>
2018-01-05 11:43:30 -0800

    Remove commented out code

--- app/controllers/api/referrals_controller.rb
+++ app/controllers/api/referrals_controller.rb
@@ -4,20 +4,7 @@ module Api
   class ReferralsController < ActionController::API
     def referrals_by_user
       referrals = Referral::ReferralRepository.new.referrals_by_user_id params[:user_id]
-      puts referrals
       render json: referrals
     end
-    # def referrals_by_user
-    #   conn = Faraday.new(:url => 'http://case-api:8080') do |faraday|
-    #     faraday.adapter Faraday.default_adapter
-    #   end
-
-    #   res = conn.get do |req|
-    #     req.url "/staff/#{params[:user_id]}/referrals"
-    #     req.headers['Accept'] = 'application/json'
-    #   end
-
-    #   render json: res.body
-    # end
   end
 end
